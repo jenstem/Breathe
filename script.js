edb948f61d6a21bf0e70be8b276342d619c8d7c1
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-13 16:45:57 -0400

    Add openW apiKey js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var googleMapsApiKey = "";
-var openWeatherApiKey = "";
+var openWeatherApiKey = "http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={8686e0fe4732b6b364f3c95d6dfcf09c}";

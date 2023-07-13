83641d3b744f2a347576c1ab6c5e5d7822197cfa
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-13 16:56:43 -0400

    Add geo url func getQuality js

--- script.js
+++ script.js
@@ -38,4 +38,5 @@ function citySearch(searchResults) {
 
 function getQuality(lat, lon, name) {
     var apiKeyAir = "8686e0fe4732b6b364f3c95d6dfcf09c";
+    var apiUrlAir = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKeyAir}`;
         });

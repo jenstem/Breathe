d4d1f96802442736351a1efd1f8098d4dda142bf
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-13 16:46:55 -0400

    Add var searchBtn js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var googleMapsApiKey = "";
 var openWeatherApiKey = "http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={8686e0fe4732b6b364f3c95d6dfcf09c}";
 
 var searchText = document.querySelector("#search-box");
+var searchBtn = document.querySelector("#searchButton");
